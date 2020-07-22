708b59b91aeddf391656684e3419536e0765ec91
Andrew David <56502440+andydavidlee@users.noreply.github.com>
2020-07-22 16:38:48 +1000

    fixes

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const Database = require('nedb');
-
+const { request, response } = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
-
 app.use(express.static('public'));
-
-
+app.use(bodyParser.json())
 const database = new Database('database.db');
 database.loadDatabase();
-
 app.get('/api', (request, response) => {
-    database.find({}, (err, data) => {
+    database.find({},(err, data) => {
         if(err){
             response.end();
             return;
@@ -19,16 +17,12 @@ app.get('/api', (request, response) => {
         response.json(data);
     })
 })
-
-app.get('/time', (request, response) => {
-    response.send(new Date());
-})
-
 app.post('/api', (request, response) => {
-    const data = request.body;
+    let data = request.body;
+    console.log(data);
     const timestamp = Date.now();
-    data.timestamp = timestamp;
+    console.log(timestamp);
+    data = {timestamp: timestamp, data};
     database.insert(data);
     response.json(data);
 })
-
--- public/weather.js
+++ public/weather.js
@@ -1,31 +1,30 @@
-function setup() {
+function setup(){
     let lat, lon;
-    if('geolocation' in navigator) {
+    if('geolocation' in navigator){
         navigator.geolocation.getCurrentPosition(position => {
             lat = position.coords.latitude;
             lon = position.coords.longitude;
             console.log(lat, lon);
             document.getElementById('latitude').textContent = lat;
             document.getElementById('longitude').textContent = lon;
-        });
-    } else {
-        console.log('geolocation not available.');
+        })
+    }else {
+        console.log('geolocation not available');
     }
-
     const button = document.getElementById('submit');
     button.addEventListener('click', async event => {
         const data = {lat, lon};
-        const option = {
+        const options = {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/JSON'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
-        }
+        };
+        console.log(data);
         const response = await fetch('http://localhost:3000/api', options);
         const json = await response.json();
         console.log(json);
     })
 }
-
 setup();
